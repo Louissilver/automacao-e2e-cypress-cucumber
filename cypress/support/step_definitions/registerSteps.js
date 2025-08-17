19ac26b06490c1dbdb872c0719e02d3a69a5ac14
Luís Fernando da Silveira <luisfernandosilveira23@gmail.com>
2025-08-17 20:42:52 -0300

    refactor: remoção de código desnecessário

--- cypress/support/step_definitions/registerSteps.js
+++ cypress/support/step_definitions/registerSteps.js
@@ -2,19 +2,8 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import RegisterPage from "../pages/registerPage";
 import { registerInformation } from "../factories/registerInformation";
 
-const random = () => Math.floor(Math.random() * 1e6);
-const validEmail = () => `user_${Date.now()}_${random()}@example.com`;
-
 Given("I access the application", () => {
   RegisterPage.openBase();
-});
-
-Given("I access the {string} page", (pageName) => {
-  if (pageName.toLowerCase() === "register") {
-    RegisterPage.goToRegister();
-  } else {
-    throw new Error(`Unknown page: ${pageName}`);
-  }
 });
 
 When("I view the registration form", () => {
